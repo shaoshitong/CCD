f7238072e032adcce61006a33011405dbd46fb7b
shaoshitong <1090784053@qq.com>
2022-05-20 10:41:52 +0800

    update origin kd 3090

--- C100/train_student_kd.py
+++ C100/train_student_kd.py
@@ -7,7 +7,6 @@ import torch.nn.functional as F
 import os,sys
 print(os.getcwd())
 sys.path.append(os.path.join(os.getcwd()))
-
 import shutil
 import argparse
 import numpy as np
@@ -29,23 +28,23 @@ scaler=torch.cuda.amp.GradScaler()
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR Training')
-parser.add_argument('--data', default='/data/data/', type=str, help='Dataset directory')
+parser.add_argument('--data', default='./data/', type=str, help='Dataset directory')
 parser.add_argument('--dataset', default='cifar100', type=str, help='Dataset name')
 parser.add_argument('--arch', default='wrn_16_2', type=str, help='student network architecture')
 parser.add_argument('--tarch', default='wrn_40_2', type=str, help='teacher network architecture')
-parser.add_argument('--tcheckpoint', default='/home/Bigdata/ckpt/ilsvrc2012/teacher/wrn_40_2.pth', type=str, help='pre-trained weights of teacher')
+parser.add_argument('--tcheckpoint', default='C:/Users/aurora_A/.cache/torch/hub/checkpoints/wrn_40_2.pth', type=str, help='pre-trained weights of teacher')
 parser.add_argument('--init-lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--weight-decay', default=1e-4, type=float, help='weight decay')
 parser.add_argument('--lr-type', default='multistep', type=str, help='learning rate strategy')
-parser.add_argument('--milestones', default=[150,180,210], type=list, help='milestones for lr-multistep')
+parser.add_argument('--milestones', default=[300,360,420], type=list, help='milestones for lr-multistep')
 parser.add_argument('--sgdr-t', default=300, type=int, dest='sgdr_t',help='SGDR T_0')
 parser.add_argument('--warmup-epoch', default=0, type=int, help='warmup epoch')
-parser.add_argument('--epochs', type=int, default=240, help='number of epochs to train')
-parser.add_argument('--batch-size', type=int, default=128, help='batch size')
-parser.add_argument('--num-workers', type=int, default=8, help='the number of workers')
+parser.add_argument('--epochs', type=int, default=480, help='number of epochs to train')
+parser.add_argument('--batch-size', type=int, default=256, help='batch size')
+parser.add_argument('--num-workers', type=int, default=4, help='the number of workers')
 parser.add_argument('--gpu-id', type=str, default='0')
 parser.add_argument('--manual_seed', type=int, default=0)
-parser.add_argument('--kd-T', type=float, default=4, help='temperature for KD distillation')
+parser.add_argument('--kd-T', type=float, default=3, help='temperature for KD distillation')
 parser.add_argument('--kd-alpha', type=float, default=0.5, help='temperature for KD distillation')
 parser.add_argument('--kd-weight', type=float, default=2, help='temperature for KD distillation')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
@@ -59,7 +58,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
 
 log_txt = 'result/'+ 'tarch' + '_' +  args.tarch + '_'+\
           'arch' + '_' +  args.arch + '_'+\
-          'dataset' + '_' +  args.dataset + '_'+f'_2倍_{args.i}' +'_4.txt'
+          'dataset' + '_' +  args.dataset + '_'+f'_2倍_{args.i}' +'.txt'
 
 log_dir = str(os.path.basename(__file__).split('.')[0]) + '_'+\
           'tarch' + '_' +  args.tarch + '_'+\
@@ -90,17 +89,17 @@ trainset = torchvision.datasets.CIFAR100(root=args.data, train=True, download=Tr
                                             transforms.Normalize([0.5071, 0.4867, 0.4408],
                                                                 [0.2675, 0.2565, 0.2761])
                                         ]))
-trainset = PolicyDatasetC100(trainset)
+# trainset = PolicyDatasetC100(trainset)
 testset = torchvision.datasets.CIFAR100(root=args.data, train=False, download=True,
                                         transform=transforms.Compose([
                                             transforms.ToTensor(),
                                             transforms.Normalize([0.5071, 0.4867, 0.4408],
                                                                 [0.2675, 0.2565, 0.2761]),
                                         ]))
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True,num_workers=8,
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True,
                                     pin_memory=(torch.cuda.is_available()))
 
-testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False,num_workers=4,
+testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False,
                                     pin_memory=(torch.cuda.is_available()))
 
 
@@ -272,7 +271,7 @@ if __name__ == '__main__':
     best_acc = 0.  # best test accuracy
     start_epoch = 0  # start from epoch 0 or last checkpoint epoch
     criterion_cls = nn.CrossEntropyLoss()
-    criterion_div = losses.SinglePPKDLoss(temperature=args.kd_T, alpha=args.kd_alpha)
+    criterion_div = losses.KDLoss(temperature=args.kd_T, alpha=args.kd_alpha)
 
     if args.evaluate:
         print('load pre-trained weights from: {}'.format(os.path.join(args.checkpoint_dir, str(model.__name__) + '.pth.tar')))     
