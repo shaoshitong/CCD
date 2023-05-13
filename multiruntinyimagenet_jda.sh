b5e8498aa725945c343eb02a67b64b16c2ca627a
shaoshitong <1090784053@qq.com>
2023-05-13 16:48:50 +0800

    shell for running tiny-imagenet

--- multiruntinyimagenet_jda.sh
+++ multiruntinyimagenet_jda.sh
@@ -2,12 +2,38 @@
 
 python ./TinyImageNet/train_student_kd.py      \
   --checkpoint-dir ./store/  \
-  --gpu 0  --i 1 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/
-#
-#python ./TinyImageNet/train_student_kd.py      \
-#  --checkpoint-dir ./store/  \
-#  --gpu 0  --i 2 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/
-#
-#python ./TinyImageNet/train_student_kd.py      \
-#  --checkpoint-dir ./store/  \
-#  --gpu 0  --i 3 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/
+  --gpu 0  --i 1 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/ --weight 1,1,0,0
+
+python ./TinyImageNet/train_student_kd.py      \
+  --checkpoint-dir ./store/  \
+  --gpu 0  --i 2 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/ --weight 1,1,0,0
+
+python ./TinyImageNet/train_student_kd.py      \
+  --checkpoint-dir ./store/  \
+  --gpu 0  --i 3 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/ --weight 1,1,0,0
+
+
+python ./TinyImageNet/train_student_kd.py      \
+  --checkpoint-dir ./store/  \
+  --gpu 0  --i 1 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/ --weight 1,1,1,1
+
+python ./TinyImageNet/train_student_kd.py      \
+  --checkpoint-dir ./store/  \
+  --gpu 0  --i 2 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/ --weight 1,1,1,1
+
+python ./TinyImageNet/train_student_kd.py      \
+  --checkpoint-dir ./store/  \
+  --gpu 0  --i 3 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/ --weight 1,1,1,1
+
+
+python ./TinyImageNet/train_student_kd.py      \
+  --checkpoint-dir ./store/  \
+  --gpu 0  --i 1 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/ --weight 1,0,1,0
+
+python ./TinyImageNet/train_student_kd.py      \
+  --checkpoint-dir ./store/  \
+  --gpu 0  --i 2 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/ --weight 1,0,1,0
+
+python ./TinyImageNet/train_student_kd.py      \
+  --checkpoint-dir ./store/  \
+  --gpu 0  --i 3 --data /home/Bigdata/tiny-imagenet/tiny-imagenet-200/ --weight 1,0,1,0
